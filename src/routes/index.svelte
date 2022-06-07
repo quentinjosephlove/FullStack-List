1455ba9ea4ef357eaaefc46e3c462ffb6e4b017a
quentinjosephlove <tchudjoquentin@yahoo.com>
2022-06-07 08:51:06 +0000

    ChangesMade

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,8 +1,18 @@
 <script>
     import TodoItem from "src/lib/todo-item.svelte";
+    const title = "Todo"
 </script>
-<h1>Welcome to SvelteKit</h1>
+
+<style>
+    
+</style>
+<svelte:head>
+    <title>{title}</title>
+</svelte:head>
+<div class="todo">
+    <h1>Welcome to SvelteKit</h1>
 <form method="" action="">
     <input type="text" placeholder="" aria-label="">
 </form>
 <TodoItem></TodoItem>
+</div>
