32c7dade661c1de6702704b21692a79a6174e640
quentinjosephlove <tchudjoquentin@yahoo.com>
2022-06-19 07:57:00 +0000

    configuration6

--- src/routes/todos/index.json.ts
+++ src/routes/todos/index.json.ts
@@ -1,6 +1,17 @@
-export const get = ()=>{
+import type {RequestHandler} from "@sveltejs/kit"
+export const get: RequestHandler = ()=>{
     return {
         status: 200,
         body: "Hello form the API"
     }
+}
+
+export const post: RequestHandler<{}, FormData> = (request) =>{
+    console.log(request.body.get("text"));
+
+    return {
+        status: 200,
+        body: request.body.get("text")
+    }
+    
 }
